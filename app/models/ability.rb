e2de128bc4446859e9b8749261e6e8ded11a014b
Serg Skol <sps676@gmail.com>
2023-03-28 17:09:00 +0200

    Improve security

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,19 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    return unless user.present?
+
+    can :read, Group, author_id: user.id
+    can :read, Payment, author_id: user.id
+
+    can :manage, Group, author_id: user.id
+    can :manage, Payment, author_id: user.id
+
+    return unless user.admin?
+
+    can :manage, :all
+  end
 end
