792faee74f25cd4ba92371cfc4c3fa39cb57b6e6
Serg Skol <sps676@gmail.com>
2023-03-28 20:12:12 +0200

    Add styling for category page

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,8 +1,4 @@
 const showMenu = () => {
   var modal = document.querySelector('.nav-menu')
-  if (modal.classList.contains('hide')) {
-    modal.classList.remove('hide');
-  } else {
-    modal.classList.add('hide');
-  }
+  modal.classList.toggle('hide');
 }
